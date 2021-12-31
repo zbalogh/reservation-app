814fbcff59c61e7bd46b59c075e3be6677b0dfc8
Zoltan Balogh <zbal@freemail.hu>
2021-12-31 13:18:22 +0100

    added API Gateway Java project based on Spring Cloud API Gateway

---
+++ project-sources/spring-api-gateway/src/main/java/com/zbalogh/reservation/apigateway/reservationapigateway/ReservationApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.zbalogh.reservation.apigateway.reservationapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Simple API Gateway for Desk Reservation Application.
+ */
+@SpringBootApplication
+public class ReservationApiGatewayApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ReservationApiGatewayApplication.class, args);
+	}
+
+}
